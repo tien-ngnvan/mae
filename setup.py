23e9eb587c15824e2a2e20c8ea15152b0ee3842f
datnvt <nguyenvanthanhdat1810@gmail.com>
2024-11-16 15:24:35 +0700

    Chore: test find package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='mae',
     version='0.1',
-    packages=find_packages(),
+    packages=find_packages("src"),
     install_requires=[
         # Add your dependencies here
     ],
