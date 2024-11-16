c5b962bef7ac2af8cad471b36ed527b044b5ff91
datnvt <nguyenvanthanhdat1810@gmail.com>
2024-11-16 16:07:40 +0700

    Fix: fix mae not found

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='mae',
     # version='0.1',
-    packages=find_packages(include=['mae', 'mae.*']),
+    packages=find_packages(where='.'),
     install_requires=[
         # Add your dependencies here
     ],
