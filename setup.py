914ac6d90ac2bf930ae3f1f64ea3730c00b80697
datnvt <nguyenvanthanhdat1810@gmail.com>
2024-11-16 14:22:00 +0700

    Feat: add setup.py for pip install repo

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mae',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Add your dependencies here
+    ],
+    description='A mae package',
+    python_requires='>=3.9',
+)
